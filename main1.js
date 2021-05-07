b2c0dd62599f3ea6729cb193d522e7231b6c562c
KID <50666207+KID421@users.noreply.github.com>
2021-05-07 10:54:06 +0800

    第一版改版

---
+++ main1.js
@@ -0,0 +1,19 @@
+window.onload = function() {
+    var navbar = document.getElementById("navbar");
+    var btn1 = document.getElementById("btn1");
+    var btn2 = document.getElementById("btn2");
+
+    btn1.addEventListener("mouseenter", function() {
+        navbar.style.height = "200px";
+    });
+    btn1.addEventListener("mouseleave", function() {
+        navbar.style.height = "15vh";
+    });
+
+    btn2.addEventListener("mouseenter", function() {
+        navbar.style.height = "300px";
+    });
+    btn2.addEventListener("mouseleave", function() {
+        navbar.style.height = "15vh";
+    });
+}
