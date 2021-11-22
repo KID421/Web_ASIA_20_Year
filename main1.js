21632972a9adc6d3ce838794ce9c6fd6f7adf76c
KID <50666207+KID421@users.noreply.github.com>
2021-11-22 22:36:29 +0800

    添加選單與效果

--- main1.js
+++ main1.js
@@ -1,19 +1,29 @@
 window.onload = function() {
+    // var btn1 = document.getElementById("btn1");
+    // var btn2 = document.getElementById("btn2");
+    var box = document.getElementById("box");
     var navbar = document.getElementById("navbar");
-    var btn1 = document.getElementById("btn1");
-    var btn2 = document.getElementById("btn2");
+    var menu = document.getElementById("menu-a");
 
-    btn1.addEventListener("mouseenter", function() {
-        navbar.style.height = "20vh";
-    });
-    btn1.addEventListener("mouseleave", function() {
-        navbar.style.height = "15vh";
-    });
+    // btn1.addEventListener("mouseenter", function() {
+    //     navbar.style.height = "20vh";
+    // });
+    // btn1.addEventListener("mouseleave", function() {
+    //     navbar.style.height = "15vh";
+    // });
 
-    btn2.addEventListener("mouseenter", function() {
-        navbar.style.height = "33vh";
+    // btn2.addEventListener("mouseenter", function() {
+    //     navbar.style.height = "33vh";
+    // });
+    // btn2.addEventListener("mouseleave", function() {
+    //     navbar.style.height = "15vh";
+    // });
+
+    menu.addEventListener("click", function() {
+        navbar.style.transform = "scaleY(1)";
+        navbar.style.transition = "0.35s";
     });
-    btn2.addEventListener("mouseleave", function() {
-        navbar.style.height = "15vh";
+    box.addEventListener("click", function() {
+        navbar.style.transform = "scaleY(0)";
     });
 }
