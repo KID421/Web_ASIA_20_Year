37b0785f4ec156aa12bc424aff5b979828a375c9
KID <50666207+KID421@users.noreply.github.com>
2021-11-27 15:16:38 +0800

    Update 選單右上角並添加關閉圖示

--- main1.js
+++ main1.js
@@ -1,4 +1,4 @@
-window.onload = function() {
+window.onload = function () {
     // var btn1 = document.getElementById("btn1");
     // var btn2 = document.getElementById("btn2");
     var box = document.getElementById("box");
@@ -19,11 +19,23 @@ window.onload = function() {
     //     navbar.style.height = "15vh";
     // });
 
-    menu.addEventListener("click", function() {
-        navbar.style.transform = "scaleY(1)";
+    var close = false;
+
+    menu.addEventListener("click", function () {
+        
         navbar.style.transition = "0.35s";
+        if (close == false) {
+            menu.style.backgroundImage = "url(./img/close.png)";
+            close = true;
+            navbar.style.transform = "scaleY(1)";
+        }
+        else if (close == true) {
+            menu.style.backgroundImage = "url(./img/menu.png)";
+            close = false;
+            navbar.style.transform = "scaleY(0)";
+        }
     });
-    box.addEventListener("click", function() {
-        navbar.style.transform = "scaleY(0)";
+    box.addEventListener("click", function () {
+        // navbar.style.transform = "scaleY(0)";
     });
 }
